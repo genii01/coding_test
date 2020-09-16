1f30d0ae0737a7353ac318bb698fadc9492e27fa
seungbum03 <ssb3258@naver.com>
2020-09-17 00:56:37 +0900

    [Q03-Q04] upload

---
+++ sb/Q_03_reorder-data-in-log-files.py
@@ -0,0 +1,18 @@
+# 로그파일 재정렬
+def reorderLogFiles(logs) :
+    letters, digits = [], []
+    for log in logs :
+        if log.split()[1].isdigit() :
+            digits.append(log)
+        else :
+            letters.append(log)
+            
+    letters.sort(key = lambda x : (x.split()[1:], x.split()[0]))
+    return letters + digits
+
+logs = ['dig1 8 1 5 1','let1 art can','dig2 3 6','let2 own kit dig','let3 art zero']
+
+result = reorderLogFiles(logs)
+print(result)
+
+
---
+++ sb/Q_04_most-common-word.py
@@ -0,0 +1,20 @@
+
+# Q_04_most-common-word.py
+from typing import List
+import re
+import collections
+paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+banned = ["hit"]
+
+class wordCounter :
+    def mostCommonWord(self, paragraph:str, banned:List[str]) -> str:
+        words = [word for word in re.sub(r'[^\w]',' ',paragraph).lower().split() if word not in banned]
+        counts = collections.Counter(words)
+        
+        return counts.most_common(1)[0][0]
+
+wc = wordCounter()
+result = wc.mostCommonWord(paragraph, banned)
+print(result)
+
+
